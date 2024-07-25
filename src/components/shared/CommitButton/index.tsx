a5ed299e496961590346c8afbbe4d08cd84182b2
simonarsen <simonyanarsn.dp@gmail.com>
2024-07-25 11:30:23 +0400

    add-some-spaces-for-commit

--- src/components/shared/CommitButton/index.tsx
+++ src/components/shared/CommitButton/index.tsx
@@ -8,6 +8,6 @@ export const CommitButton: React.FC<CommitButtonProps> = React.memo(({ handleRef
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
         Refresh
-      </button>
+      </button> 
     );
 })
